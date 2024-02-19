e7f16324f3e4894e989b4d2bb91a650e379d3df9
ducankdz <ducanh21112003>
2024-02-19 23:15:19 +0700

    Thêm package responses

---
+++ Sneakers/src/main/java/com/example/Sneakers/responses/OrderResponse.java
@@ -0,0 +1,61 @@
+package com.example.Sneakers.responses;
+
+import com.example.Sneakers.models.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResponse {
+    private Long id;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("fullname")
+    private String fullName;
+
+    private String email;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    private String address;
+
+    private String note;
+
+    @JsonProperty("order_date")
+    private LocalDateTime orderDate;
+    
+    private String status;
+
+    @JsonProperty("total_money")
+    private Float totalMoney;
+
+    @JsonProperty("shipping_method")
+    private String shippingMethod;
+
+    @JsonProperty("shipping_address")
+    private String shippingAddress;
+
+    @JsonProperty("shipping_date")
+    private LocalDate shippingDate;
+
+    @JsonProperty("tracking_number")
+    private String trackingNumber;
+
+    @JsonProperty("payment_method")
+    private String paymentMethod;
+
+    private Boolean active;
+}
---
+++ Sneakers/src/main/java/com/example/Sneakers/responses/ProductListResponse.java
@@ -0,0 +1,15 @@
+package com.example.Sneakers.responses;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductListResponse {
+    private List<ProductResponse> products;
+    private int totalPages;
+}
